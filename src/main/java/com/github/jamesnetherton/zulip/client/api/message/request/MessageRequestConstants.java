1463667170a247939a10af3947bfaace55cbd449
Sungho Park <mrchaos@naver.com>
2021-10-15 11:08:46 +0900

    Update MessageRequestConstants.java
    change private class to public class

--- src/main/java/com/github/jamesnetherton/zulip/client/api/message/request/MessageRequestConstants.java
+++ src/main/java/com/github/jamesnetherton/zulip/client/api/message/request/MessageRequestConstants.java
@@ -1,19 +1,16 @@
 package com.github.jamesnetherton.zulip.client.api.message.request;
 
-final class MessageRequestConstants {
+public class MessageRequestConstants {
 
-    final static String MARK_ALL_AS_READ = "mark_all_as_read";
-    final static String MARK_STREAM_AS_READ = "mark_stream_as_read";
-    final static String MARK_TOPIC_AS_READ = "mark_topic_as_read";
-    final static String MESSAGES_API_PATH = "messages";
-    final static String MESSAGES_ID_API_PATH = MESSAGES_API_PATH + "/%d";
-    final static String FLAGS_API_PATH = MESSAGES_API_PATH + "/flags";
-    final static String HISTORY_API_PATH = MESSAGES_ID_API_PATH + "/history";
-    final static String MATCHES_NARROW_API_PATH = MESSAGES_API_PATH + "/matches_narrow";
-    final static String REACTIONS_API_PATH = MESSAGES_ID_API_PATH + "/reactions";
-    final static String RENDER_MESSAGE_API_PATH = MESSAGES_API_PATH + "/render";
-    final static String USER_UPLOADS_API_PATH = "user_uploads";
-
-    private MessageRequestConstants() {
-    }
+    public static final String MARK_ALL_AS_READ = "mark_all_as_read";
+    public static final String MARK_STREAM_AS_READ = "mark_stream_as_read";
+    public static final String MARK_TOPIC_AS_READ = "mark_topic_as_read";
+    public static final String MESSAGES_API_PATH = "messages";
+    public static final String MESSAGES_ID_API_PATH = MESSAGES_API_PATH + "/%d";
+    public static final String FLAGS_API_PATH = MESSAGES_API_PATH + "/flags";
+    public static final String HISTORY_API_PATH = MESSAGES_ID_API_PATH + "/history";
+    public static final String MATCHES_NARROW_API_PATH = MESSAGES_API_PATH + "/matches_narrow";
+    public static final String REACTIONS_API_PATH = MESSAGES_ID_API_PATH + "/reactions";
+    public static final String RENDER_MESSAGE_API_PATH = MESSAGES_API_PATH + "/render";
+    public static final String USER_UPLOADS_API_PATH = "user_uploads";
 }
