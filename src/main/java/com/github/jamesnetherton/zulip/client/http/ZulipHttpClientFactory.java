f39250ff198db07b5d28b1889e86d31c86be8b20
James Netherton <jamesnetherton@gmail.com>
2021-10-23 08:06:39 +0100

    Add JavaDoc to ZulipHttpClientFactory

--- src/main/java/com/github/jamesnetherton/zulip/client/http/ZulipHttpClientFactory.java
+++ src/main/java/com/github/jamesnetherton/zulip/client/http/ZulipHttpClientFactory.java
@@ -2,6 +2,11 @@ package com.github.jamesnetherton.zulip.client.http;
 
 import com.github.jamesnetherton.zulip.client.exception.ZulipClientException;
 
+/**
+ * Factory class for creating {@link ZulipHttpClient} implementations.
+ *
+ * Provides a way to use HTTP client libraries other than the default Apache Commons HTTP Client.
+ */
 public interface ZulipHttpClientFactory {
     ZulipHttpClient createZulipHttpClient(ZulipConfiguration configuration) throws ZulipClientException;
 }
--- src/main/java/com/github/jamesnetherton/zulip/client/http/commons/ZulipCommonsHttpClientFactory.java
+++ src/main/java/com/github/jamesnetherton/zulip/client/http/commons/ZulipCommonsHttpClientFactory.java
@@ -5,6 +5,10 @@ import com.github.jamesnetherton.zulip.client.http.ZulipConfiguration;
 import com.github.jamesnetherton.zulip.client.http.ZulipHttpClient;
 import com.github.jamesnetherton.zulip.client.http.ZulipHttpClientFactory;
 
+/**
+ * An {@link ZulipHttpClientFactory} implementation that creates a {@link ZulipHttpClient} based on
+ * the Apache Commons HTTP Client.
+ */
 public class ZulipCommonsHttpClientFactory implements ZulipHttpClientFactory {
 
     @Override
